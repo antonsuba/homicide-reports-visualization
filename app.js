e62e41a41f9a8d40d69548d2ff3bc419f6201a1f
Anton Suba <antonsuba@outlook.com>
2017-05-17 17:57:50 +0800

    Add support for multiple d3 versions

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ let firstChartArray = [[],[]];
 // Initialize First Chart
 //=========================
 function renderFirstChart(){
-    d3.csv('data/CrimeByRelationshipBySex.csv', (d) => {
+    // d3_v3 = d3_v3_v3;
+
+    d3_v3.csv('data/CrimeByRelationshipBySex.csv', (d) => {
         return {
             victim_sex: d.victim_sex,
             acquaintance_friend: d.Acquaintance_Friend,
@@ -32,7 +34,7 @@ function renderFirstChart(){
             right:70
         }
 
-        let crimeByRelationshipBySex = d3.nest()
+        let crimeByRelationshipBySex = d3_v3.nest()
             .key((d) => d.victim_sex)
             .entries(data);
 
@@ -46,20 +48,20 @@ function renderFirstChart(){
             firstChartArray[i].sort((a,b) => b.value - a.value);
         }
 
-        let svg = d3.select('#relationship-graph').append('svg')
+        let svg = d3_v3.select('#relationship-graph').append('svg')
             .attr("width", width)
             .attr("height", height)
             .attr('transform', `translate(${margin.left}, 0)`);
 
-        let x = d3.scale.linear()
+        let x = d3_v3.scale.linear()
             .domain([0, maxData])
             .range([0, width - margin.right]);
 
-        let y = d3.scale.ordinal()
+        let y = d3_v3.scale.ordinal()
             .rangeRoundBands([0, height], .1)
             .domain(firstChartArray[firstChartState].map(d => d.key));
 
-        let yAxis = d3.svg.axis()
+        let yAxis = d3_v3.svg.axis()
             .scale(y)
             .tickSize(0)
             .orient('left');
@@ -92,12 +94,12 @@ function renderFirstChart(){
             .duration(1500)
             .attr("x", d => x(d.value) + 10);
 
-        d3.select('#chart1-male-btn').on('click', () => {
+        d3_v3.select('#chart1-male-btn').on('click', () => {
             let widthArray = firstChartArray[1].map(d => x(d.value));
             updateHorizontalChart(firstChartArray[1], widthArray);
         });
 
-        d3.select('#chart1-female-btn').on('click', () => {
+        d3_v3.select('#chart1-female-btn').on('click', () => {
             let widthArray = firstChartArray[0].map(d => x(d.value));
             updateHorizontalChart(firstChartArray[0], widthArray);
         });
@@ -108,7 +110,7 @@ function renderFirstChart(){
 function updateHorizontalChart(dataSet, widthArray){
     
     let counter = -1;
-    let bars = d3.select("#relationship-graph").selectAll('.bar');
+    let bars = d3_v3.select("#relationship-graph").selectAll('.bar');
 
     bars.transition()
         .duration(1500)
@@ -119,7 +121,7 @@ function updateHorizontalChart(dataSet, widthArray){
 
     dataSetCounter = -1;
     counter = -1;
-    let labels = d3.select("#relationship-graph").selectAll('.label');
+    let labels = d3_v3.select("#relationship-graph").selectAll('.label');
 
     labels.transition()
         .duration(1500)
@@ -133,14 +135,110 @@ function updateHorizontalChart(dataSet, widthArray){
         });
 }
 
-renderFirstChart();
-
 
 //=========================
 // Initialize Second Chart
 //=========================
 function renderSecondChart(){
+    let svg = d3_v4.select("#race-sex-svg"),
+        margin = {top: 100, right: 100, bottom: 30, left: 20},
+        width = +svg.attr("width"),
+        height = +svg.attr("height") ,
+        g = svg.append("g").attr("transform", "translate(" + margin.right + "," + 50 + ")");
+
+
+    let y = d3_v4.scaleBand()
+        .rangeRound([0, width-200])
+        .padding(0.6)
+        .align(0.15);
+
+    let x = d3_v4.scaleLinear()
+        .rangeRound([900-200, 0]);
+
+    let vert = d3_v4.scaleBand()
+        .domain(["White Male", "White Female", "Native American/Alaska Native-Male", "Native American/Alska Native-Female","Black-Male","Black-Female", "Asian/Pacific Islander-Male","Asian/Pacific Islander-Female",])
+        .rangeRound([0, width-200])
+        .padding(0.6)
+        .align(0.15);
+
+    let yAxis = d3_v4.axisRight()
+        .tickSizeOuter(0)
+        .scale(vert)
+        
+
+    let z = d3_v4.scaleOrdinal()
+        .range(["#0c0101", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ebf442","#364cbc"]);
+
+    let stack = d3_v4.stack()
+        .offset(d3_v4.stackOffsetExpand);
+
+
+
+    d3_v4.csv("data/VictimPerpetratorRaceAndSex.csv", function(error, data) {
+        if (error) throw error;
+        console.log(data);
+        data.sort(function(a, b) { return b[data.columns[1]] / b.total - a[data.columns[1]] / a.total; });
+
+        y.domain(data.map(function(d) { return d.sex; }));
+        z.domain(data.columns.slice(1));
     
+        
+    let serie = g.selectAll(".serie")
+        .data(stack.keys(data.columns.slice(1))(data))
+        .enter().append("g")
+        .attr("class", "serie")
+        .attr("fill", function(d) { return z(d.key); });
+
+    serie.selectAll("rect")
+        .data(function(d) { return d; })
+        .enter().append("rect")
+        .attr("y", function(d) { return y(d.data.sex); })
+        .attr("x", function(d) { return x(d[1]); })
+        .attr("width", function(d) { return x(d[0]) - x(d[1]); })
+        .attr("height", y.bandwidth())
+        .attr("transform", "translate(0," + 0 + ")");
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .call(d3_v4.axisTop(x).ticks(10,"%"))
+        .attr("transforn", "scale(-1,1)")
+        .style("text-anchor", "end")
+        .selectAll("text")  
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-180)");
+    
+    g.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(880," + 0 + ")")
+        .attr("transform", "rotate(180,450,400)")
+        .call(yAxis)
+
+    let legend = g.selectAll(".legend")
+        .data(data.columns.slice(1).reverse())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; })
+        .style("font", "10px sans-serif");
+
+    legend.append("rect")
+        .attr("x", width + 25)
+        .attr("transform", "translate(-1120,"+ 800+ ")")
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("fill", z);
+
+    legend.append("text")
+        // .attr("x", width + 25)
+        // .attr("y", 9)
+        //.attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .text(function(d) { return d; })
+        .attr("transform", "translate(200,"+ 5+ ")")
+        .attr("transform", "rotate(180,15,402)");
+        
+    });
 }
 
 
@@ -148,6 +246,8 @@ function renderSecondChart(){
 // Initialize Third Chart
 //=========================
 function renderThirdChart(){
+    // d3_v3 = d3_v3_v3;
+
     let active = 0;
             
     function mapbutton1(){
@@ -174,37 +274,37 @@ function renderThirdChart(){
 
     let dataVisualized = 0;
             
-    let canvas = d3.select('#murder-rate-graph').append('svg')
+    let canvas = d3_v3.select('#murder-rate-graph').append('svg')
                     .attr('width', 900)
                     .attr('height', 500)
                     .attr('align', 'center');
 
     let group = canvas.append('g').attr('class', 'states');
 
-    let states = d3.map();
+    let states = d3_v3.map();
 
-    let color1 = d3.scale.linear()
+    let color1 = d3_v3.scale.linear()
                     .domain([30,80])
                     .range(['#ff8c8c', '#724432']);
 
-    let color2 = d3.scale.linear()
+    let color2 = d3_v3.scale.linear()
                     .domain([0,10])
                     .range(['#689ef9', '#233a60']);
                     
-    let color3 = d3.scale.linear()
+    let color3 = d3_v3.scale.linear()
                     .domain([1,13])
                     .range(['#d1a262', '#4c391f']);
 
-    let projection = d3.geo.albersUsa().scale(1000).translate([480, 250]);
-    let path = d3.geo.path().projection(projection);
-    let tooltip = d3.select('body').append('div').attr('id', 'tooltip').attr('class', 'hidden');
+    let projection = d3_v3.geo.albersUsa().scale(1000).translate([480, 250]);
+    let path = d3_v3.geo.path().projection(projection);
+    let tooltip = d3_v3.select('body').append('div').attr('id', 'tooltip').attr('class', 'hidden');
 
     let usMapData;
     let murderByState;
 
-    d3.queue()
-        .defer(d3.json, "data/USA.geojson")
-        .defer(d3.csv, "data/MurdersByState.csv", function(d){
+    d3_v3.queue()
+        .defer(d3_v3.json, "data/USA.geojson")
+        .defer(d3_v3.csv, "data/MurdersByState.csv", function(d){
             states.set(d.state, [+d.GunMurderPercentage, +d.GunMurdersPer100000, +d.MurdersPer100000])
         })
         .await(makeMap);
@@ -243,8 +343,8 @@ function renderThirdChart(){
             })
             .on('mousemove', function(d,i){
                 console.log('hit');
-                d3.select(this).style('fill-opacity', 0.7);
-                let mouse = d3.mouse(canvas.node()).map(function(d){
+                d3_v3.select(this).style('fill-opacity', 0.7);
+                let mouse = d3_v3.mouse(canvas.node()).map(function(d){
                     return parseInt(d);
                 });
                 tooltip.classed('hidden', false)
@@ -258,7 +358,7 @@ function renderThirdChart(){
                             'from 1980-2014.');
             })
             .on('mouseout', function(d,i){
-                d3.selectAll('.states path').style('fill-opacity', 1);
+                d3_v3.selectAll('.states path').style('fill-opacity', 1);
                 tooltip.classed('hidden', true);
             });
     }
@@ -271,8 +371,8 @@ function renderThirdChart(){
             })
             .on('mousemove', function(d,i){
                 console.log('hit');
-                d3.select(this).style('fill-opacity', 0.7);
-                let mouse = d3.mouse(canvas.node()).map(function(d){
+                d3_v3.select(this).style('fill-opacity', 0.7);
+                let mouse = d3_v3.mouse(canvas.node()).map(function(d){
                     return parseInt(d);
                 });
                 tooltip.classed('hidden', false)
@@ -286,7 +386,7 @@ function renderThirdChart(){
                             'from 1980-2014');
             })
             .on('mouseout', function(d,i){
-                d3.selectAll('.states path').style('fill-opacity', 1);
+                d3_v3.selectAll('.states path').style('fill-opacity', 1);
                 tooltip.classed('hidden', true);
             });
     }
@@ -299,8 +399,8 @@ function renderThirdChart(){
             })
             .on('mousemove', function(d,i){
                 console.log('hit');
-                d3.select(this).style('fill-opacity', 0.7);
-                let mouse = d3.mouse(canvas.node()).map(function(d){
+                d3_v3.select(this).style('fill-opacity', 0.7);
+                let mouse = d3_v3.mouse(canvas.node()).map(function(d){
                     return parseInt(d);
                 });
                 tooltip.classed('hidden', false)
@@ -314,8 +414,12 @@ function renderThirdChart(){
                             'from 1980-2014.');
             })
             .on('mouseout', function(d,i){
-                d3.selectAll('.states path').style('fill-opacity', 1);
+                d3_v3.selectAll('.states path').style('fill-opacity', 1);
                 tooltip.classed('hidden', true);
             });
     }
 }
+
+renderFirstChart();
+renderSecondChart();
+renderThirdChart();
